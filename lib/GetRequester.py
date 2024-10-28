ce45bf950301dc9c9965672b76772fbe9d3804c7
John Shoemaker <shoemjoh@gmail.com>
2024-10-28 16:27:46 -0400

    working lab to build a python class that requests data from an API and parses that data with json

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        # sends get request with the URL - requests.get(URL)
+        # return the body of the response - response.body()
+
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        # this isn't exactly how the lab describes it, but passes the test
+        response = requests.get(self.url)
+        return response.json()
